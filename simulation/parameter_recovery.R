287843b8793355d6a121706c8312ea7fde70486b
malte_lueken <malte_lueken@arcor.de>
2020-05-07 17:54:13 +0200

    update parameter recovery

--- simulation/parameter_recovery.R
+++ simulation/parameter_recovery.R
@@ -6,90 +6,60 @@
 library(depmixS4)
 library(parallel)
 source("simulation/model_simulation.R")
+source("algorithm/model_helper_functions.R")
 
 
 # Part 1 ------------------------------------------------------------------
 
-D <- 100
+D <- 5
 N <- 2500
 K <- 2:4
 
 aij <- seq(0.01, 0.99, length.out = D)
 
-alpha.vel <- matrix(NA, D, max(K))
-beta.vel <- matrix(NA, D, max(K))
-alpha.acc <- matrix(NA, D, max(K))
-beta.acc <- matrix(NA, D, max(K))
+par.int.true <- cbind(seq(1, 5, length.out = D),
+                      seq(0.1, 0.6, length.out = D),
+                      seq(1, 5, length.out = D),
+                      seq(0.05, 0.25, length.out = D),
+                      seq(1, 5, length.out = D),
+                      seq(5, 25, length.out = D),
+                      seq(1, 5, length.out = D),
+                      seq(1, 5, length.out = D),
+                      1/seq(0.1, 10, length.out = D),
+                      seq(1, 5, length.out = D),
+                      seq(1, 5, length.out = D),
+                      seq(1, 5, length.out = D),
+                      seq(1, 3, length.out = D),
+                      1/seq(0.1, 10, length.out = D),
+                      seq(1, 5, length.out = D),
+                      seq(1, 5, length.out = D),
+                      seq(1, 3, length.out = D),
+                      seq(1, 3, length.out = D),
+                      1/seq(0.1, 10, length.out = D))
 
-alpha.vel[,1] <- seq(1, 5, length.out = D)
-beta.vel[,1] <- seq(0.1, 0.6, length.out = D)
-alpha.acc[,1] <- seq(1, 5, length.out = D)
-beta.acc[,1] <- seq(0.05, 0.25, length.out = D)
 
-alpha.vel[,2] <- seq(1, 5, length.out = D)
-beta.vel[,2] <- seq(5, 25, length.out = D)
-alpha.acc[,2] <- seq(1, 5, length.out = D)
-beta.acc[,2] <- seq(1, 5, length.out = D)
+par.fix.true <- list(fix = list(vel = c(3, 0.35), acc = c(3, 0.15)), 
+                     sac = list(vel = c(3, 15), acc = c(3, 3), angle = c(0, 1)),
+                     pso = list(vel = c(3, 3), acc = c(2, 2), angle = c(pi, 1)),
+                     sp = list(vel = c(3, 1.5), acc = c(3, 0.15), angle = c(0, 1)))
 
-alpha.vel[,3] <- seq(1, 5, length.out = D)
-beta.vel[,3] <- seq(1, 5, length.out = D)
-alpha.acc[,3] <- seq(1, 5, length.out = D)
-beta.acc[,3] <- seq(1, 3, length.out = D)
-
-alpha.vel[,4] <- seq(1, 5, length.out = D)
-beta.vel[,4] <- seq(1, 5, length.out = D)
-alpha.acc[,4] <- seq(1, 3, length.out = D)
-beta.acc[,4] <- seq(1, 3, length.out = D)
-
-kappa <- 1/seq(0.1, 10, length.out = D)
-
-alpha.vel.fix <- c(3, 3, 3, 3)
-beta.vel.fix <- c(0.35, 15, 3, 1.5)
-alpha.acc.fix <- c(3, 3, 3, 3)
-beta.acc.fix <- c(0.15, 3, 2, 0.15)
-
-mu.fix <- c(0, pi, 0)
-kappa.fix <- 1
-
-par.true <- cbind(seq(1, 5, length.out = D),
-                  seq(0.1, 0.6, length.out = D),
-                  seq(1, 5, length.out = D),
-                  seq(0.05, 0.25, length.out = D),
-                  seq(1, 5, length.out = D),
-                  seq(5, 25, length.out = D),
-                  seq(1, 5, length.out = D),
-                  seq(1, 5, length.out = D),
-                  1/seq(0.1, 10, length.out = D),
-                  seq(1, 5, length.out = D),
-                  seq(1, 5, length.out = D),
-                  seq(1, 5, length.out = D),
-                  seq(1, 3, length.out = D),
-                  1/seq(0.1, 10, length.out = D),
-                  seq(1, 5, length.out = D),
-                  seq(1, 5, length.out = D),
-                  seq(1, 3, length.out = D),
-                  seq(1, 3, length.out = D),
-                  1/seq(0.1, 10, length.out = D))
-
-respstart <- list(fix = list(vel = c(alpha.vel.fix[1], beta.vel.fix[1]), 
-                                          acc = c(alpha.acc.fix[1], beta.acc.fix[1])), 
-                               sac = list(vel = c(alpha.vel.fix[2], beta.vel.fix[2]), 
-                                          acc = c(alpha.acc.fix[2], beta.acc.fix[2]),
-                                          angle = c(mu.fix[1], kappa.fix)))
-
-trstart <- matrix(1/2, nrow = 2, ncol = 2)
-instart <- rep(1/2, 2)
+par.states <- c(rep(1, 4), rep(2:4, each = 5))
+par.names <- c("alpha", "beta", "alpha", "beta", rep(c("alpha", "beta", "alpha", "beta", "kappa"), 3))
+par.resp <- c(rep(1:2, each = 2), rep(c(1, 1, 2, 2, 3),  3))
 
 ncores <- detectCores()
 clust <- makeCluster(ncores)
-clusterExport(clust, list("HMM_simulate", "N", "respstart", "trstart", "instart"))
+clusterExport(clust, list("HMM_simulate", "N", "par.fix.true"))
                                
-estimates <- parLapply(clust, alpha.vel[,1], function(x, N = 500, k = 2, 
-                                                      start1 = respstart, start2 = trstart, start3 = instart) {
+estimates <- parLapply(clust, par.int.true[,1], function(x, N = 500, K = 2, k = 2, par = "alpha", met = 1, true = par.fix.true) {
   
-  respstart[[1]][[1]][1] <- x
+  par.fix.true[[k]][[met]][1] <- 3
   
-  model <- HMM_simulate(n = N, nstates = k, respstart = start1, trstart = start2, instart = start3)
+  instart <- rep(1/K, K)
+  
+  trstart <- matrix(1/K, nrow = K, ncol = K)
+  
+  model <- HMM_simulate(n = N, nstates = k, respstart = true, trstart = trstart, instart = instart)
   
   sim <- simulate(model)
   
